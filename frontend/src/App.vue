4791416ed1244ae82f526311a1d63c8fdc73e0a0
Niclas van Eyk <niclas.eyk@gmail.com>
2025-07-11 21:50:11 +0200

    error handling + some dev chaos
    mostly to get a bit more familiar with Vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,27 +2,55 @@
 import { onMounted, reactive } from 'vue';
 import { client } from './lib/api';
 
-const state = reactive({ logs: ([] as string[]) });
+const state = reactive({
+  logs: ([] as string[]),
+  hasError: false,
+  isLoading: false,
+ });
 
-onMounted(async () => {
-  const { data, error } = await client.GET("/v1/logs");
-  if (data && data.lines) {
-    state.logs = data.lines;
-  } else {
-    // TODO: show toast or something
-    console.error(error);
+async function loadLogs() {
+  state.isLoading = true;
+  state.hasError = false;
+  try {
+    const { data, error } = await client.GET("/v1/logs");
+    if (data && data.lines) {
+      state.hasError = false;
+      state.logs = data.lines;
+      return
+    }
+
+    if (error) {
+      state.hasError = true;
+    }
+  } catch (error) {
+    state.hasError = true;
+  } finally {
+    state.isLoading = false;
   }
-})
+}
+
+onMounted(loadLogs);
 </script>
 
 <template>
   <div class="h-screen w-screen flex place-content-center flex-wrap flex-col gap-16 dark:text-white dark:bg-black">
     <h1 class="font-mono text-5xl font-black text-center">LOGS</h1>
 
-    <div class="h-1/2 w-3/4 overflow-scroll flex flex-col border-2 dark:border-white p-2">
-      <span class="font-mono whitespace-pre text-sm" v-for="line in state.logs">
+    <div
+      class="h-1/2 w-3/4 overflow-scroll flex flex-col border-2 dark:border-white p-2"
+      :class="{'bg-red-100 text-red-900 dark:bg-red-900 dark:text-red-100': state.hasError}"
+    >
+      <span
+        v-if="!state.hasError"
+        v-for="line in state.logs"
+        class="font-mono whitespace-pre text-sm"
+      >
         {{ line }}
       </span>
+      <div v-else class="flex flex-col gap-6 bg-red h-full w-full flex-wrap place-content-center">
+        Something went wrong!
+        <button class="px-4 py-2 bg-red-200 dark:bg-red-800 rounded" v-on:click="loadLogs">try again</button>
+      </div>
     </div>
   </div>
 </template>
--- frontend/src/lib/api/index.ts
+++ frontend/src/lib/api/index.ts
@@ -3,3 +3,15 @@ import type { paths } from "./v1";
 
 export const client = createClient<paths>({ baseUrl: "/api" });
 
+if (import.meta.env.DEV) {
+    client.use({
+        async onRequest({ request, options }) {
+            // Simulates failures when in dev
+            if (Math.random() > 0.8) {
+                await new Promise((resolve) => setTimeout(resolve, 1_000));
+                throw new Error("Test");
+            }
+            return request;
+        },
+    });
+}
